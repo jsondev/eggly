5d0957e04fa76d6dd5f613e63ff4c66a05952ffd
James Anderson <jsondev1.0@outlook.com>
2018-05-15 09:10:59 -0400

    adding state param service

--- app/categories/bookmarks/bookmarks.js
+++ app/categories/bookmarks/bookmarks.js
@@ -1,6 +1,23 @@
-angular.module('categories.bookmarks',[
+angular.module('categories.bookmarks', [
     'categories.bookmarks.create',
     'categories.bookmarks.edit',
-    'eggly.models.categories', 
+    'eggly.models.categories',
     'eggly.models.bookmarks'
-]);
+])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('eggly.categories.bookmarks', {
+                url: 'categories/',
+                views: {
+                    'bookmarks@': {
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html',
+                        controller: 'BookmarksCtrl'
+                    }
+                }
+            })
+    })
+    .controller('BookmarksCtrl', function ($scope, $stateParams) {
+        $scope.currentCategoryName = $stateParams.category;
+    })
+
+    ;
